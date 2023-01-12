683dad9f93b2cc8a58e34ddbea05eee68f223b38
BlackSwipe <121950178+BlackSwipe@users.noreply.github.com>
2023-01-12 09:52:18 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function scrollToElement(products) {
 let valueDisplays = document.querySelectorAll(".num");
 let interval = 4000;
 
-const observer1 = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
@@ -43,122 +43,53 @@ valueDisplays.forEach((valueDisplay) => {
   }, duration);
 });
 
-const observer2 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-const observer3 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-const observer4 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-const observer5 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-const observer6 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-const observer7 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-const observer8 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-const observer9 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
 const hiddenElements1 = document.querySelectorAll(".info-box");
-hiddenElements1.forEach((el) => observer2.observe(el));
+hiddenElements1.forEach((el) => observer.observe(el));
 
 const hiddenElements2 = document.querySelectorAll(".feature-box");
-hiddenElements2.forEach((el) => observer1.observe(el));
+hiddenElements2.forEach((el) => observer.observe(el));
 
 const hiddenElements3 = document.querySelectorAll(".icon-section");
-hiddenElements3.forEach((el) => observer3.observe(el));
+hiddenElements3.forEach((el) => observer.observe(el));
 
 const hiddenElements4 = document.querySelectorAll(".category-box");
-hiddenElements4.forEach((el) => observer4.observe(el));
+hiddenElements4.forEach((el) => observer.observe(el));
 
 const hiddenElements5 = document.querySelectorAll(".review-box");
-hiddenElements5.forEach((el) => observer5.observe(el));
+hiddenElements5.forEach((el) => observer.observe(el));
 
 const hiddenElements6 = document.querySelectorAll(".header");
-hiddenElements6.forEach((el) => observer6.observe(el));
+hiddenElements6.forEach((el) => observer.observe(el));
 
 const hiddenElements7 = document.querySelectorAll(".product-box");
-hiddenElements7.forEach((el) => observer7.observe(el));
+hiddenElements7.forEach((el) => observer.observe(el));
 
 const hiddenElements8 = document.querySelectorAll(".product-header");
-hiddenElements8.forEach((el) => observer7.observe(el));
+hiddenElements8.forEach((el) => observer.observe(el));
 
 const hiddenElements9 = document.querySelectorAll(".product-pricing");
-hiddenElements9.forEach((el) => observer7.observe(el));
+hiddenElements9.forEach((el) => observer.observe(el));
 
 const hiddenElements10 = document.querySelectorAll(".category-header");
-hiddenElements10.forEach((el) => observer8.observe(el));
+hiddenElements10.forEach((el) => observer.observe(el));
 
 const hiddenElements11 = document.querySelectorAll(".GUI-cheat");
-hiddenElements11.forEach((el) => observer9.observe(el));
+hiddenElements11.forEach((el) => observer.observe(el));
 
 const hiddenElements12 = document.querySelectorAll(".hero");
-hiddenElements12.forEach((el) => observer9.observe(el));
+hiddenElements12.forEach((el) => observer.observe(el));
 
 const hiddenElements13 = document.querySelectorAll(".faq-header");
-hiddenElements13.forEach((el) => observer9.observe(el));
+hiddenElements13.forEach((el) => observer.observe(el));
 
 const hiddenElements14 = document.querySelectorAll(".status-header");
-hiddenElements14.forEach((el) => observer9.observe(el));
+hiddenElements14.forEach((el) => observer.observe(el));
 
 const hiddenElements15 = document.querySelectorAll(".status-table");
-hiddenElements15.forEach((el) => observer9.observe(el));
+hiddenElements15.forEach((el) => observer.observe(el));
+
+const hiddenElements16 = document.querySelectorAll(".status-conditions");
+hiddenElements16.forEach((el) => observer.observe(el));
 
 function decreaseQuantity() {
   var currentQuantity = parseInt(document.getElementById("quantity").innerHTML);
